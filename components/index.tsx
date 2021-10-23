ce7f8f1aa2e11587eee42ce9cb6c587997223737
ntoulemont <n.toulemont@gmail.com>
2021-10-23 18:05:06 +0200

    adding in mobile nav

--- components/index.tsx
+++ components/index.tsx
@@ -4,6 +4,7 @@ export * from './layouts/Main'
 export * from './layouts/Category'
 export * from './layouts/Post'
 export * from './layouts/SideBarNav'
+export * from './layouts/MobileNav'
 
 // Mdx specifics
 export * from './mdx/DragToReOrder'
