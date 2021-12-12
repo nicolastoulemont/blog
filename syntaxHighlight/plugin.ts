d839ee85476ada6cf6cd02b174facc1285efaef7
ntoulemont <n.toulemont@gmail.com>
2021-12-12 22:28:09 +0100

    feat: fix build

--- syntaxHighlight/plugin.ts
+++ syntaxHighlight/plugin.ts
@@ -1,10 +1,11 @@
 import { createRemarkPlugin } from '@atomiks/mdx-pretty-code'
 import fs from 'fs'
+import path from 'path'
 
 export const syntaxHighLightPlugin = createRemarkPlugin({
   // Options passed to shiki.getHighlighter()
   shikiOptions: {
     // Link to your VS Code theme JSON file
-    theme: JSON.parse(fs.readFileSync(require.resolve('./dark.json'), 'utf-8')),
+    theme: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'syntaxHighlight', 'dark.json'), 'utf-8')),
   },
 })
