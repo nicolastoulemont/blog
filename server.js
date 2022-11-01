847478cc32b03bb05e96abffd3f6b74cd961c2a9
ntoulemont <n.toulemont@gmail.com>
2022-11-01 15:12:17 +0100

    feat: forgot to save

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { createRequestHandler } = require("@remix-run/vercel")
-module.exports = createRequestHandler({
-  build: require("./build"),
-})
+import { createRequestHandler } from "@remix-run/vercel"
+import * as build from "@remix-run/dev/server-build"
+
+export default createRequestHandler({ build, mode: process.env.NODE_ENV })
