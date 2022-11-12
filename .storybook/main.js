c97636fdd3e69be27cc5ea898f2597733894e1dd
ntoulemont <n.toulemont@gmail.com>
2022-11-12 20:33:43 +0100

    feat: framer motion storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,4 +2,13 @@ module.exports = {
   stories: ["../app/**/*.stories.mdx", "../app/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    })
+
+    return config
+  },
 }
