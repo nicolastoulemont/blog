b954ab7e6f025434499226f42fd7e02e9d41a5c6
ntoulemont <n.toulemont@gmail.com>
2022-11-13 12:02:16 +0100

    feat: better ToC styles

--- app/components/TableOfContent/Mobile.tsx
+++ app/components/TableOfContent/Mobile.tsx
@@ -1,10 +1,12 @@
 import { useMemo, useState } from "react"
 import { useTableOfContent } from "./useTableOfContent"
 import type { TableOfContentProps } from "./types"
-import { TableOfContentLink } from "./Link"
 import { FiMenu, FiX } from "react-icons/fi"
 import { motion, AnimatePresence, Variants } from "framer-motion"
 import { Dialog } from "@headlessui/react"
+import { Link, useNavigate } from "@remix-run/react"
+import { CATEGORY_COLOR_VARIANTS } from "~/utils/styles"
+import { TableOfContentLinkProps } from "./types"
 
 const MotionPanel = motion(Dialog.Panel)
 
@@ -62,7 +64,7 @@ export function MobileTableOfContent({ elements, activeColor }: TableOfContentPr
                 <nav className="max-h-[70vh] overflow-scroll px-6 pb-6 pt-2">
                   <ul>
                     {elements.map((element, index) => (
-                      <TableOfContentLink
+                      <MobileLink
                         key={`${element.content}${element.id}${index}`}
                         element={element}
                         activeColor={activeColor}
@@ -79,5 +81,61 @@ export function MobileTableOfContent({ elements, activeColor }: TableOfContentPr
         )}
       </AnimatePresence>
     </>
+  )
+}
+
+const styles = {
+  h1: {
+    spacing: 0,
+    size: "text-lg",
+  },
+  h2: {
+    spacing: 0,
+    size: "text-lg",
+  },
+  h3: {
+    spacing: "0.5rem",
+    size: "text-base",
+  },
+  h4: {
+    spacing: "1rem",
+    size: "text-sm",
+  },
+  h5: {
+    spacing: "1.25rem",
+    size: "text-xs",
+  },
+  h6: {
+    spacing: "1.5rem",
+    size: "text-[11px]",
+  },
+} as const
+
+export function MobileLink({ element, activeColor, isActive, isFirst, onClose = () => {} }: TableOfContentLinkProps) {
+  const { bg, text, hoverBg, hoverText } = CATEGORY_COLOR_VARIANTS[activeColor]
+  const navigate = useNavigate()
+
+  function handleClick() {
+    // Headless UI Dialog prevent regular navigation with the link
+    navigate(`#${element.id}`)
+    onClose()
+  }
+
+  return (
+    <li className="my-3 flex" style={{ marginLeft: styles[element.type].spacing }}>
+      <Link
+        className={`${styles[element.type].size} ${isActive ? `${bg} ${text}` : ""} 
+        w-full rounded-md p-2 font-medium text-gray-700 dark:text-white ${hoverBg} ${hoverText}`}
+        to={`#${element.id}`}
+        onClick={handleClick}
+        style={{
+          transitionTimingFunction: "ease-in-out",
+          transitionProperty: "color, background-color",
+          transitionDuration: "0.3s",
+        }}
+      >
+        {isFirst ? "Top" : element.content}
+      </Link>
+    </li>
   )
 }
