2caec561eba07c6d23d229d0ff5a383567f94c69
ntoulemont <n.toulemont@gmail.com>
2022-11-20 15:41:28 +0100

    feat: improved the main input styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -40,7 +40,7 @@ export default function Index() {
           id="search"
           name="search"
           type="text"
-          className="w-full rounded-lg bg-white text-slate-800 placeholder:text-slate-800 dark:bg-slate-500"
+          className="w-full rounded-lg bg-white text-slate-800 placeholder:text-slate-800 dark:border-black dark:bg-slate-900 dark:text-white dark:placeholder:text-white"
           placeholder="Search posts"
           onChange={(e) => submit(e.currentTarget.form)}
           {...(params.search && params.search !== "" && { defaultValue: params.search })}
