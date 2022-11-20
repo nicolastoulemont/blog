6e09321ed07e7caca22a78aa91eb41c3093f7728
ntoulemont <n.toulemont@gmail.com>
2022-11-20 18:32:20 +0100

    docs: more comments

--- app/components/DatePicker/components/Provider/init.ts
+++ app/components/DatePicker/components/Provider/init.ts
@@ -11,11 +11,17 @@ export function init({ value }: Partial<DatePickerProps>): State {
     /** The value given back to the consumer of the DatePicker */
     value,
     /**
-     * Use as an internal date reference to populate the calendar UI
+     * Used as an internal date reference to populate the calendar UI
      * and move across years and months without impacting the value selected itself
      */
     calendarDate,
+    /**
+     * Used to manage animation directions
+     */
     slideDir: "none",
+    /**
+     * Used to manage fill the year view
+     */
     yearRange: [new Date().getFullYear() - 2, new Date().getFullYear() + 9],
   }
 }
