1fffb2acb4ed39ed106a4630b1851b3fe086513c
ntoulemont <n.toulemont@gmail.com>
2023-02-05 20:17:28 +0100

    fix: added root page to sitemap

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -2,8 +2,28 @@ import * as Posts from "~/utils/files"
 
 export const loader = () => {
   const posts = Posts.getAll()
+
+  const postsByDates = posts.sort((a, b) =>
+    new Date(a["article:published_time"]).getTime() >
+    new Date(b["article:published_time"]).getTime()
+      ? -1
+      : new Date(a["article:published_time"]).getTime() <
+        new Date(b["article:published_time"]).getTime()
+      ? 1
+      : 0
+  )
+
+  const lastPost = postsByDates[0]
+
   const content = `
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+            <loc>https://nicolastoulemont.dev</loc>
+            <lastmod>${new Date(
+              lastPost["article:published_time"]
+            ).toISOString()}</lastmod>
+            <priority>1.0</priority>
+        </url>
       ${posts
         .map(
           (post) => `<url>
